15f5e4a3c7afd0d9eeea83c30def15c4709e8fd1
Ian Zhang <ianzh4ng@gmail.com>
2024-10-13 01:45:14 -0700

    Rename example files for Music Favourites plugin setup

--- src/main/java/com/musicfavourites/MusicFavouritesPlugin.java
+++ src/main/java/com/musicfavourites/MusicFavouritesPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.musicfavourites;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Music Favourites"
 )
-public class ExamplePlugin extends Plugin
+public class MusicFavouritesPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private MusicFavouritesConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	MusicFavouritesConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(MusicFavouritesConfig.class);
 	}
 }
--- src/test/java/com/musicfavourites/MusicFavouritesPluginTest.java
+++ src/test/java/com/musicfavourites/MusicFavouritesPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.musicfavourites;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MusicFavouritesPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MusicFavouritesPlugin.class);
 		RuneLite.main(args);
 	}
 }
